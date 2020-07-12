96e6735371c8d1a148d4f88dce1c91627f14342a
Joshua Pollak <josh@freda.pico.org>
2020-07-12 13:01:13 -0700

    fix dot/comma

--- keyboards/planck/keymaps/pyrogenic/keymap.c
+++ keyboards/planck/keymaps/pyrogenic/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_LEFT_PAREN, KC_RIGHT_PAREN,  KC_UP, KC_SCOLON ,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_LEFT_PAREN, KC_RIGHT_PAREN,  KC_UP, KC_COMMA ,
     LT(_PLOVER, KC_GRAVE), KC_LCTL, KC_LALT, KC_LGUI, LT(_LOWER, KC_SPACE),   KC_SPC,  KC_SPC,  LT(_RAISE, KC_SPACE), KC_MINUS, KC_LEFT, KC_DOWN,   KC_RGHT
 ),
 
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     _______, KC_1, KC_2,   KC_3, KC_4,  KC_5, KC_6, KC_7,    KC_8,    KC_9, KC_0, KC_DELETE,
     _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,    LALT(KC_LBRACKET),    LALT(KC_RBRACKET), KC_DOUBLE_QUOTE, _______,
-    _______, KC_3,   _______,   _______,   _______,  _______,  _______,  KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_PGUP,  _______,
+    _______, KC_3,   _______,   _______,   _______,  _______,  _______,  KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_PGUP,  KC_DOT,
     KC_AT, _______, _______, _______, _______, _______, _______, KC_HASH,    KC_PLUS,    LALT(KC_LEFT), KC_PGDOWN, LALT(KC_RIGHT)
 ),
 
