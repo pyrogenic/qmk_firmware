4730a3f7792853bdecf80ed239922ee39f953ba4
Joshua Pollak <abottomlesspit@gmail.com>
2020-08-23 17:46:31 -0700

    working tapdance and backlight

--- keyboards/bm43a/keymaps/pyrogenic/keymap.c
+++ keyboards/bm43a/keymaps/pyrogenic/keymap.c
@@ -1,33 +1,30 @@
-/* Copyright 2019 mechmerlin
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
+#include "quantum.h"
+
+// Tap Dance declarations
+enum tapdances {
+    TD_QUOTE_OR_ENTER,
+    // TD_L_OR_BACKSLASH,
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for quote, twice for enter
+    [TD_QUOTE_OR_ENTER] = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_ENT),
+    // [TD_L_OR_BACKSLASH] = ACTION_TAP_DANCE_DOUBLE(KC_L, KC_BSLS),
+};
+
+// // Add tap dance item in place of a key code
+// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//     // ...
+//     TD(TD_ESC_CAPS)
+//     // ...
+// };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,                 KC_SPC,              MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-    [1] = LAYOUT(
-        RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,   BL_TOGG, BL_DEC,  BL_INC,  BL_STEP, _______, _______, _______, _______, _______, _______,
-        _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,
-        _______, _______, _______, _______,                _______,             _______, _______, _______, _______
-    ),
-
+	[0] = LAYOUT(KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, TD(TD_QUOTE_OR_ENTER), KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_UP, KC_DOT, LCTL_T(KC_SCLN), LALT_T(KC_SLSH), LGUI_T(KC_MINS), LT(2,KC_SPC), LT(3,KC_SPC), MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
+	[1] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_MINS, KC_EQL, KC_DEL, KC_TRNS, BL_TOGG, BL_OFF, BL_INC, BL_DEC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_QUOT, KC_TRNS, RGB_TOG, RGB_M_T, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_HOME, KC_RBRC, RESET, KC_TRNS, KC_EQL, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_END, KC_PGDN),
+	[2] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, LGUI(KC_TAB), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_MUTE, KC_RPRN, KC_TRNS, KC_TRNS, KC_EQL, KC_TRNS, KC_TRNS, LGUI(KC_LBRC), KC_MRWD, KC_MPLY, KC_MFFD),
+	[3] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, KC_TRNS, KC_F11, KC_F12, KC_F13, KC_F14, KC_F15, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_TRNS, KC_RCBR, KC_TRNS, KC_TRNS, KC_EQL, KC_TRNS, KC_TRNS, LGUI(KC_RBRC), KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
---
+++ keyboards/bm43a/keymaps/pyrogenic/rules.mk
@@ -0,0 +1,6 @@
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
